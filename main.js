f277a6f07f8024acf6e98afa0026d64755cdd38c
Shiraito Yuno <haruno.konoha.music@gmail.com>
2024-10-19 00:30:21 +0900

    はちよちよ！！！

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-const alert = () => {
-    alert("はちよちよ！");
+const alert_tonality = (num) => {
+    if(num==-1){
+        alert("へちよちよ！");
+    }
+    if(num==0){
+        alert("はちよちよ！");
+    }
 }
